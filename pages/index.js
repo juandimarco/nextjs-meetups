03545542da24260dfb30a0c932ccc344f2a3f9ec
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-07-04 00:31:55 -0300

    Add Static Site Generation

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,21 @@ const DUMMY_MEETUPS = [
   }
 ];
 
-function HomePage() {
+function HomePage(props) {
   return (
-    <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={props.meetups} />
   );
+};
+
+export async function getStaticProps() {
+  // fetch data from an API
+  
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  };
 };
 
 export default HomePage;
